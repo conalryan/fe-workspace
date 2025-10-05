7adf290fe17219d88d91addaf19dabdd5e198d69
Conal Ryan <corya@akamai.coms>
2025-10-04 21:38:38 -0400

    feat(basic-reat-app): provider

---
+++ packages/basic-react-app/src/providers/basic.provider.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useState, useContext } from 'react';
+
+interface BasicContextType {
+  baseCtx: string;
+  setBaseCtx: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const BasicContext = createContext<BasicContextType>({
+  baseCtx: '',
+  setBaseCtx: () => {},
+});
+
+export const BasicProvider: React.FC<React.PropsWithChildren<{}>> = ({
+  children,
+}) => {
+  const [baseCtx, setBaseCtx] = useState('Initial Value');
+  return (
+    <BasicContext.Provider value={{ baseCtx, setBaseCtx }}>
+      {children}
+    </BasicContext.Provider>
+  );
+};
+
+export const BasicComponent = () => {
+  const { baseCtx, setBaseCtx } = useContext(BasicContext);
+  return (
+    <div>
+      <p>{baseCtx}</p>
+      <button onClick={() => setBaseCtx('Updated Value')}>Update</button>
+    </div>
+  );
+};
