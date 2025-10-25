ccfeeb0d088c0002d76fce15337a030a4537cbf3
Conal Ryan <corya@akamai.coms>
2025-10-24 21:37:40 -0400

    chore(basic-react-remote): lint and format

--- libs/module-federation/basic-react-remote/src/App.tsx
+++ libs/module-federation/basic-react-remote/src/App.tsx
@@ -1,29 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import reactLogo from './assets/react.svg';
+
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a
+          href="https://vite.dev"
+          target="_blank">
+          <img
+            alt="Vite logo"
+            className="logo"
+            src={viteLogo}
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="https://react.dev"
+          target="_blank">
+          <img
+            alt="React logo"
+            className="logo react"
+            src={reactLogo}
+          />
         </a>
       </div>
       <h1>Basic React Remote</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- libs/module-federation/basic-react-remote/vite.config.ts
+++ libs/module-federation/basic-react-remote/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { federation } from '@module-federation/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 import { dependencies } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     federation({
-      filename: "remoteEntry.js",
-      name: "remote",
       exposes: {
-        "./remote-app": "./src/App.tsx",
+        './remote-app': './src/App.tsx',
       },
+      filename: 'remoteEntry.js',
+      name: 'remote',
       remotes: {},
       shared: {
         react: {
