4eb8aa6b836c1cd9d61a819982672797c146c58a
Conal Ryan <corya@akamai.coms>
2025-11-15 10:43:59 -0500

    feat(angular-mf-host): Add sass support

--- libs/module-federation/angular-mf-host/rsbuild.config.ts
+++ libs/module-federation/angular-mf-host/rsbuild.config.ts
@@ -1,14 +1,17 @@
 import { createConfig } from '@ng-rsbuild/plugin-angular';
+import { pluginSass } from '@rsbuild/plugin-sass';
 import applyFederation from './federation.config';
 
 const rsbuildConfig = createConfig(
   {
     browser: './src/main.ts',
+    styles: ['./src/styles.scss'],
   },
   {
     server: {
       port: 4200,
     },
+    plugins: [pluginSass()],
   }
 );
 
